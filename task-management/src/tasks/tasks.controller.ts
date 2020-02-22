5849111e5e7f00f70cae9a418a731253615da45d
Travis Overby <travisoverby@gmail.com>
2020-02-21 22:31:22 -0800

    Finish REST API for task management app; use pipes for validation and error handling

--- task-management/src/tasks/tasks.controller.ts
+++ task-management/src/tasks/tasks.controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    }
     return this.tasksService.getAllTasks();
   }
 
@@ -18,6 +23,7 @@ export class TasksController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
   }
@@ -26,4 +32,13 @@ export class TasksController {
   deleteTask(@Param('id') id: string): void {
     this.tasksService.deleteTask(id);
   }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string, 
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus
+  ) {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+
 }
